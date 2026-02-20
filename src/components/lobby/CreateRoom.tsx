dcfbd305cf349bf4a6a52b7aea8df935396ec423
pavel-gabriel <pavel.gabriell@yahoo.com>
2026-02-20 21:37:14 +0200

    Update CreateRoom.tsx

--- src/components/lobby/CreateRoom.tsx
+++ src/components/lobby/CreateRoom.tsx
@@ -52,7 +52,7 @@ export default function CreateRoom() {
         operations: showMathOptions ? operations : ['+'],
         rounds,
         timePerRound,
-        shapeMode: isShapeMatch ? shapeMode : undefined,
+        ...(isShapeMatch && { shapeMode }),
       }, avatar);
       await createRoomInDB(room);
       registerDisconnectCleanup(room.id, true);
